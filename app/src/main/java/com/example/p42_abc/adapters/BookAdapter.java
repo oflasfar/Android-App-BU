8bc439b2545d0453d752d24c14ad7d08c19614f4
Cl1ient <teekzie68@gmail.com>
2026-03-13 21:12:35 +0100

    Ajout de BookAdapter et BookViewHolder

---
+++ app/src/main/java/com/example/p42_abc/adapters/BookAdapter.java
@@ -0,0 +1,25 @@
+package com.example.p42_abc.adapters;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/p42_abc/adapters/BookViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.p42_abc.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BookViewHolder extends RecyclerView.ViewHolder {
+    public BookViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
