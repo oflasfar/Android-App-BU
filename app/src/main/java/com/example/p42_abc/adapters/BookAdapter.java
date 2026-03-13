370d57d060bdbb21a6f20967d6bb7f3082e59e64
Cl1ient <teekzie68@gmail.com>
2026-03-13 22:38:32 +0100

    Implémentation de BookListFragment et mise à jour de BookAdapter

--- app/src/main/java/com/example/p42_abc/adapters/BookAdapter.java
+++ app/src/main/java/com/example/p42_abc/adapters/BookAdapter.java
@@ -5,7 +5,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.p42_abc.models.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
+
+    private List<Book> _bookList = new ArrayList<>();
 
     @NonNull
     @Override
@@ -22,4 +29,10 @@ public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
     public int getItemCount() {
         return 0;
     }
+
+    public void setBooks(List<Book> books){
+        this._bookList = books;
+        notifyDataSetChanged();
+    }
+
 }
--- app/src/main/java/com/example/p42_abc/views/BookListFragment.java
+++ app/src/main/java/com/example/p42_abc/views/BookListFragment.java
@@ -1,6 +1,54 @@
 package com.example.p42_abc.views;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.p42_abc.R;
+import com.example.p42_abc.adapters.BookAdapter;
+import com.example.p42_abc.viewmodels.BookViewModel;
 
 public class BookListFragment extends Fragment {
+    private BookViewModel bookViewModel;
+    private BookAdapter bookAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // lier le fragment a fragment_book_list
+        return inflater.inflate(R.layout.fragment_book_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // init RecyclerView et Adapter
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_books);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        bookAdapter = new BookAdapter();
+        recyclerView.setAdapter(bookAdapter);
+
+        // init le ViewModel
+        bookViewModel = new ViewModelProvider(this).get(BookViewModel.class);
+
+        // on observe les nouveaus books
+        bookViewModel.getBooks().observe(getViewLifecycleOwner(), books -> {
+            // on envoie a l'adapter pour l'afficher ensuite dans la view
+            if (books != null) {
+                bookAdapter.setBooks(books);
+            }
+        });
+        view.findViewById(R.id.fab_add_book).setOnClickListener(v -> {
+            // TODO : afficher fragment_add_book
+        });
+    }
 }
