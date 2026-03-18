3dda1dd5bb4a2139d9cfe3ec365f8999b23563bc
Omar <tonemail@example.com>
2026-03-18 16:01:37 +0300

    ajout du recyclerView dans l affichage des livres dans chaque auteur

--- app/src/main/java/com/example/p42_abc/author/view/AuthorDetailFragment.java
+++ app/src/main/java/com/example/p42_abc/author/view/AuthorDetailFragment.java
@@ -13,14 +13,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.p42_abc.adapters.BookAdapter;
 import com.example.p42_abc.models.Book;
 import com.example.p42_abc.R;
 import com.example.p42_abc.author.model.Author;
 import com.example.p42_abc.author.viewModel.AuthorSharedViewModel;
+import com.example.p42_abc.viewModels.BookViewModel;
 
 public class AuthorDetailFragment extends Fragment {
 
+    //Adapter pour le recycler View
+    private BookAdapter bookAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,6 +42,8 @@ public class AuthorDetailFragment extends Fragment {
 
         // On utilise bien requireActivity() pour récupérer le meme ViewModel que la liste
         AuthorSharedViewModel model = new ViewModelProvider(requireActivity()).get(AuthorSharedViewModel.class);
+        BookViewModel bookViewModel = new ViewModelProvider(requireActivity()).get(BookViewModel.class);
+
 
         model.getSelected().observe(getViewLifecycleOwner(), author -> {
             if (author != null) {
@@ -44,45 +52,34 @@ public class AuthorDetailFragment extends Fragment {
             }
         });
 
-        btnDelete.setOnClickListener(v -> {
-            //On récupère l'auteur actuellement sélectionné dans le ViewModel
-            Author currentAuthor = model.getSelected().getValue();
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_author_books);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        // On cre l'adapter en lui donnant l action de clic
+        bookAdapter = new BookAdapter(clickedBook -> {
+            bookViewModel.selectBook(clickedBook); // On prévient le ViewModel qu'on regarde CE livre
+            Navigation.findNavController(view).navigate(R.id.bookDetailFragment);
+        });
+        recyclerView.setAdapter(bookAdapter);
+
+        //On onserve le livedata des livres de l auteur
+        model.getAuthorBooks().observe(getViewLifecycleOwner(), books -> {
+            if (books != null) {
+                bookAdapter.setBooks(books);
+            }
+        });
+
+        btnDelete.setOnClickListener(v -> {
+            //On recupère l'auteur actuellement sélectionné dans le ViewModel
+            Author currentAuthor = model.getSelected().getValue();
             if (currentAuthor != null) {
-                //On demande au ViewModel de supprimer l'auteur (et ses livres)
+
                 model.deleteAuthor(currentAuthor.getId());
 
-                //On ferme le fragment de détail pour revenir à la liste
                 Navigation.findNavController(view).popBackStack();
 
-                // Petit message pour confirmer à l'utilisateur
                 Toast.makeText(getContext(), "Auteur supprimé", Toast.LENGTH_SHORT).show();
             }
-        });
-
-
-        TextView textBooksList = view.findViewById(R.id.textViewBooksList);
-
-        model.getAuthorBooks().observe(getViewLifecycleOwner(), books -> {
-            if (books != null && !books.isEmpty()) {
-                // Sil a des livres, on crée un texte avec tous les titres
-                StringBuilder liste = new StringBuilder("Livres de l'auteur :\n\n");
-                for (Book livre : books) {
-                    liste.append("- ").append(livre.getTitle())
-                            .append(" (").append(livre.getPublicationYear()).append(")\n");
-                }
-                textBooksList.setText(liste.toString());
-            } else {
-                // S il n en a pas
-                textBooksList.setText("Cet auteur n'a aucun livre enregistré.");
-            }
-        });
-
-
-        Button btnBack = view.findViewById(R.id.buttonBack);
-        btnBack.setOnClickListener(v -> {
-            // On utilise NavController pour depiler paps le putain de fragmentManager qui a casser tous mon code
-            Navigation.findNavController(view).popBackStack();
         });
     }
 }
